196065534698ae7679608ca1cffce2c5a6666b9c
Moises Munaldi <ork.mgm@gmail.com>
2023-11-30 20:41:14 +0000

    First commit

---
+++ SOFT8032A3/Template.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 29/11/2023
+
+
+Name: Moises Munaldi
+Student ID: R00225292
+Cohort: evSD3
+
+"""
+
+
+
+# Use the below few lines to ignore the warning messages
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+def warn(*args, **kwargs):
+    pass
+warnings.warn = warn
+
+
+
+
+def  task1():
+   
+    """
+    Find the number of unique locations present in the dataset. Utilize an
+    appropriate visualization technique to display the five locations with the
+    fewest records or rows. Present the percentage for each section and perform all necessary data cleaning.
+    """
+
+
+    return print ("hello world")
+
+task1()    
+    
+#def task2():
+    
+    
+
+#def task3():
+    
+    
+        
+#def task4():
+    
+    
+
+
+#def task5():
+    
+    
+
+
+
+#def task6():
+    
+    
+    
+#def task7():
+    
+
+
+
+
+
+        
